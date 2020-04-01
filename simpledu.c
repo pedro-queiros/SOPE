19bfa386e6d31cbe3c9f950e271f6e16c26bfc60
David <up201806371@fe.up.pt>
2020-04-01 16:19:12 +0100

    Fixed output

--- simpledu.c
+++ simpledu.c
@@ -45,33 +45,94 @@ void checkFlags(int size, char* argv[], struct flags *f){
     dup2(src,STDOUT_FILENO);
     if(f.all){
         printf();
-    }
+
 }*/
 
+int readReg (char* filepath){
+    struct stat file;
+    int size, size2;
 
+    if (stat(filepath, &file) < 0) {
+        printf("Error reading file stat.\n");
+        exit(1);
+    }
+    size2 = size = file.st_blocks / 2;
+    printf("%d",size);
+    int count = 0, x;
+    do
+    {
+        count++;
+        size /= 10;
+    } while(size != 0);
+    count = 8 - count;
+    for (x = 0; x < count; x++){
+        printf(" ");
+    }
+    printf("%s\n", filepath);
 
+    return size2;
+}
+
+int readDir (char* path){
+    DIR* dr;
+    struct dirent *dir;
+    char filepath[256];
+    int size = 0;
+
+    if ((dr = opendir(path)) == NULL){
+        perror(path);
+        exit(1);
+    }
+
+    while((dir = readdir(dr)) != 0){
+        if(strcmp(dir->d_name,".") == 0|| strcmp(dir->d_name,"..") == 0)
+            continue;
+        strcpy (filepath, path);
+        strcat (filepath, "/");
+        strcat (filepath, dir->d_name);
+        size += readReg(filepath);
+    }
+    return size;
+}
+
+int printDirectory (char* path, int size){
+    int count = 0, x, size2 = size;
+    do
+    {
+        count++;
+        size2 /= 10;
+    } while(size2 != 0);
+    count = 8 - count;
+    printf("%d", size);
+    for (x = 0; x < count; x++){
+        printf(" ");
+    }
+    printf("%s\n", path);
+
+    return size2;
+}
 
 int main(int argc, char* argv[], char* envp[]){
-    int src;
-    struct stat stat_buff; //file;
-    char path[PATH_MAX];
+    //  int src;
+    struct stat stat_buff;
     struct flags f = {false,false,false,false,false,false,false};
     DIR *dr;
-    struct dirent *dir;
-    //int size;
+    char path[256];
+    int size;
 
-    if(argc > 8 ){
+    if(argc > 8){
         fprintf(stderr, "Usage: %s -l [path] [-a] [-b] [-B size] [-L] [-S] [--max-depth=N]\n", argv[0]);
         exit(1);
     }
 
     strcpy(path,argv[2]);
 
-    if((src = open("output.txt",O_WRONLY | O_CREAT | O_TRUNC, 0600)) == -1){
+
+    /*if((src = open("output.txt",O_WRONLY | O_CREAT | O_TRUNC, 0600)) == -1){
         printf("Error Number % d\n", errno);
         perror("Error: ");
         exit(2);
-    }
+    }*/
 
     checkFlags(argc,argv,&f);
 
@@ -79,20 +140,15 @@ int main(int argc, char* argv[], char* envp[]){
         perror("lstat ERROR");
         exit(3);
     }
+
     if(S_ISDIR(stat_buff.st_mode)){
         dr = opendir(path);
         if(dr){
             if(f.all){
-                dup2(src,STDOUT_FILENO);
-                while((dir = readdir(dr)) != NULL){
-                    if(strcmp(dir->d_name,".") == 0|| strcmp(dir->d_name,"..") == 0)
-                        continue;
-                    //stat(dir->d_name,&file);
-                    //size = file.st_size;
-                    printf("%s/%s\n",path,dir->d_name);
-                }
-                //size = stat_buff.st_size;
-                printf("%s",path);
+                //dup2(src,STDOUT_FILENO);
+                size = readDir(path);
+                size += stat_buff.st_blocks / 2;
+                printDirectory(path, size);
                 closedir(dr);
             }
             if(f.bytes){
@@ -100,7 +156,6 @@ int main(int argc, char* argv[], char* envp[]){
             }
         }
     }
-
     exit(0);
 
 }
