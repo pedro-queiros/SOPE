db38e9062a7b4c4b8ca43164feb412d822bf7ee6
pedroqtuga <pedromsqueirs@gmail.com>
2020-04-25 15:32:59 +0100

    Separated project 1 from project 2

---
+++ Projeto2/simpledu.c
@@ -0,0 +1,21 @@
+#include "logs.h"
+#include <sys/stat.h>
+#include <errno.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include "flags.h"
+#include "utils.h"
+
+#define MAX_COMMANDS 10
+#define READ 0
+#define WRITE 1
+
+int main(int argc, char* argv[], char* envp[]){
+    
+    exit(0);
+}
